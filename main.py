fa53e1280e51fb7ecfc3f3aa1186178df6e9ce52
ANANTHMURTHY RAO <ananthmurthy.rao@siemensgamesa.com>
2021-06-17 14:27:16 +0530

    Added value = 'Hello World 6'

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Suhas Despande')
-    print_hi('Hello World')
     print_hi('Hello World 2')
     print_hi('Hello World 3')
     print_hi('Hello World 4')
